53576335b5387a39e2a25e46733fbb87ef6f4901
sam hinton <sam.hinton88@gmail.com>
2018-01-30 18:03:49 +0000

    added test for native language in settings and language modules

--- test/crud/user/user_read_test.js
+++ test/crud/user/user_read_test.js
@@ -13,6 +13,15 @@ describe('User Read', () => {
   it('reads an email address', async() => {
     const returnedUser = await User.findOne({ email }).exec();
     assert(returnedUser.email === email);
+  })
 
+  it('reads a native language', async() => {
+    const returnedUser = await User.findOne({ email }).exec();
+    assert(returnedUser.settings.nativeLanguage === 'english');
+  })
+
+  it('reads a spanish language module', async() => {
+    const returnedUser = await User.findOne({ email }).exec();
+    assert(returnedUser.languageModules[0].languageName === 'spanish');
   })
 })
