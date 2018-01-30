37cc13e816d7affdbe0fe2353527cb65e604a6b7
sam hinton <sam.hinton88@gmail.com>
2018-01-30 17:41:18 +0000

    added beginings of user read test

---
+++ test/crud/user/user_read_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const User = require('../../../models/user');
+const { email, password } = require('../../data').userData;
+
+describe('User Read', () => {
+  let user;
+
+  beforeEach( async() => {
+    user = new User({ email, password });
+    await user.save()
+  })
+
+  it('reads an email address', async() => {
+
+  })
+})
