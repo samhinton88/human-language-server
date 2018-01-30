a5a76349de6826ab23b40b4ae7f8fbbeed7e3ed6
sam hinton <sam.hinton88@gmail.com>
2018-01-30 18:07:02 +0000

    added first data to grammar data for tests

---
+++ test/data/grammar_data.js
@@ -0,0 +1,13 @@
+module.exports {
+  ar: {
+    nadar: {
+      infinitive: 'nadar',
+      grammar: {
+        person: '2',
+        count: 'sng',
+        mood: 'present'
+      },
+      conjugation: 'nadas'
+    }
+  }
+}
